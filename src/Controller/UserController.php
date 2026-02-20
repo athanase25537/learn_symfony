cb743c701f9f8827f96c394c54590c3753ceb122
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-02-20 15:17:53 +0300

    feat: symfony form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,13 +13,29 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
+
+    #[Route('/', name: 'app_users')]
+    public function main(): Response 
+    {
+        return $this->render('user/index.html.twig', ['user' => null]);
+    }
+
+    #[Route('/new-user', name: 'app_new_user')]
+    public function newUser(Request $request): Response 
+    {
+        $userForm = $request->query->get('user_form');
+        return $this->render('user/new_user.html.twig', [
+            'user_form' => $userForm,
+            'user' => null
+        ]);
+    }
+
     #[Route('/', name: 'app_user')]
     public function index(UserRepository $userRepository, Request $request): Response
     {
         $id = $request->query->get('id');
-        $user = $userRepository->find(['id' => $id]);
+        $user = $userRepository->find(['id' => $id]) ? $id : null;
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
             'user' => $user
         ]);
     }
@@ -24,13 +43,23 @@ final class UserController extends AbstractController
     #[Route('/create', name: 'app_user_create')]
     public function create(UserRepository $userRepository, \Doctrine\ORM\EntityManagerInterface $entityManager)
     {
-        $user = $userRepository->createUser($entityManager, 'Smith', 'Jane', 25, 'jane.smith@example.com');
+        $userForm = $this->createForm(UserType::class);
 
-        if ($user === null) {
-            // Handle the error, e.g., throw an exception or redirect elsewhere
-            throw $this->createNotFoundException('User could not be created.');
+        return $this->redirectToRoute('app_new_user', ['user_form' => $userForm]);
+    }
+
+    #[Route('/user/edit/{id}', name: 'app_user_edit')]
+    public function edit(User $user, Request $request, EntityManagerInterface $em)
+    {
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+        if($userForm->isSubmitted() && $userForm->isValid()) {
+            $em->flush();
         }
 
-        return $this->redirectToRoute('app_user', ['id' => $user->getId()]);
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'user_form' => $userForm
+        ]);
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('firstname')
+            ->add('age')
+            ->add('email')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Envoyer',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
